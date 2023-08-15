cc74d7a5181d84616984a25204c2735702c6529e
Mai <21039366+Mai0313@users.noreply.github.com>
2023-08-16 06:22:02 +0800

    style: 改用setup.sh 取代 requirements.txt

---
+++ setup.sh
@@ -0,0 +1,3 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install dvc-gdrive torchvision hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX timm==0.4.12 rootutils
+pip install autoroot autorootcwd ipykernel lightning matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
