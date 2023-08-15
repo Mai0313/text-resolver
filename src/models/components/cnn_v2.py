2e6844a436d1c211191c1bdd24c43b26aae62f0f
Mai <21039366+Mai0313@users.noreply.github.com>
2023-08-16 06:52:07 +0800

    feat: add md3

---
+++ src/models/components/cnn_v2.py
@@ -0,0 +1,55 @@
+from torch import nn
+
+class CaptchaNet(nn.Module):
+    def __init__(self, nc=1, leakyRelu=False):
+        super(CaptchaNet, self).__init__()
+
+        ks = [3, 3, 3, 3, 3, 3, 2]
+        ps = [1, 1, 1, 1, 1, 1, 0]
+        ss = [1, 1, 1, 1, 1, 1, 1]
+        nm = [16, 32, 64, 64, 128, 128, 128]
+
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i), nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i), nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))
+        convRelu(2, True)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d((2, 2), (2, 1), (0, 1)))
+        convRelu(4, True)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3), nn.MaxPool2d((2, 2), (2, 1), (0, 1)))
+        convRelu(6, True)
+
+        self.cnn = cnn
+
+        # Classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 2 * 1, 512), # Update input size
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 5 * 36) # 5 characters, each character has 36 possibilities
+        )
+
+    def forward(self, input):
+        x = self.cnn(input)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        x = x.view(-1, 5, 36)
+        return x
