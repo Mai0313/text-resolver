d199728b56b6941a098f3e66c38d020a36ba1a25
mai0313 <weichenglee0313@gmail.com>
2023-08-23 16:36:20 +0800

    feat: add setup script

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
-#!/bin/bash
+@echo off
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX timm==0.4.12 rootutils
 pip install autoroot autorootcwd ipykernel lightning matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
+pause
