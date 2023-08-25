95d5526ee261bad53613dd0dc73b2d07f1042c96
mai0313 <mai@mai0313.com>
2023-08-25 23:48:00 +0800

    feat: update dataset

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX timm==0.4.12 rootutils wget
+pip install dvc dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX timm==0.4.12 rootutils wget
 pip install autoroot autorootcwd ipykernel lightning matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
