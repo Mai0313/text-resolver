0eba4a21dd94018d147e0e829ce6aac6d764a3a4
mai0313 <mai@mai0313.com>
2023-08-26 18:15:04 +0800

    feat: move some input selection out to the config

--- src/models/components/cnn.py
+++ src/models/components/cnn.py
@@ -2,39 +2,48 @@ from torch import nn
 
 
 class CaptchaNet(nn.Module):
-    def __init__(self):
+    def __init__(self, 
+                 in_channels: int = 1, 
+                 kernel_size: int = 3,
+                 stride: int = 1,
+                 padding: int = 1,
+                 num_classes: int = 36, 
+                 num_chars: int = 5, 
+                 hidden_size: int = 256
+                 ):
         super().__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, hidden_size // 8, kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(hidden_size // 8, hidden_size // 4, kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(hidden_size // 4, hidden_size // 2, kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(hidden_size // 2, hidden_size, kernel_size=kernel_size, stride=stride, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(256 * 3 * 1, 512),
+            nn.Linear(hidden_size * 3 * 1, hidden_size * 2),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.Linear(hidden_size * 2, hidden_size),
             nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 5 * 36)
+            nn.Linear(hidden_size, num_chars * num_classes)
         )
+
+        self.num_chars = num_chars
+        self.num_classes = num_classes
 
     def forward(self, x):
         x = self.feature_extractor(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
-        x = x.view(-1, 5, 36)
+        x = x.view(-1, self.num_chars, self.num_classes)
         return x
 
 if __name__ == "__main__":
