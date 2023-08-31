26743a3d3d2c68d031c749777745c7df8466a534
mai0313 <mai@mai0313.com>
2023-08-31 14:05:55 +0800

    feat: improve resnet structure

--- src/models/components/resnet.py
+++ src/models/components/resnet.py
@@ -1,37 +1,101 @@
 import torch.nn as nn
-from torchvision import models
 
 
-class CaptchaResNet(nn.Module):
-    def __init__(self,
-                 in_channels: int = 3,
-                 kernel_size: int = 3,
-                 stride: int = 1,
-                 padding: int = 1,
-                 num_classes: int = 36,
-                 num_chars: int = 5,
-                 hidden_size: int = 256
-                 ):
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
         super().__init__()
-        self.resnet = models.resnet18(pretrained=False)
-        self.resnet.conv1 = nn.Conv2d(in_channels, 64, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
-        self.features = nn.Sequential(*list(self.resnet.children())[:-2])
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.batch_norm1 = nn.BatchNorm2d(out_channels)
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
+        self.batch_norm3 = nn.BatchNorm2d(out_channels * self.expansion)
+
+        self.i_downsample = i_downsample
+        self.stride = stride
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x.clone()
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+
+        x = self.conv3(x)
+        x = self.batch_norm3(x)
+
+        if self.i_downsample is not None:
+            identity = self.i_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+
+        return x
+
+class CustomResNet(nn.Module):
+    def __init__(self, num_classes=36, num_chars=5):
+        super().__init__()
+
+        self.in_channels = 64  # Initialize the in_channels
+
+        # First convolution and maxpool
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.batch_norm1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Residual blocks
+        self.layer1 = self._make_layer(Bottleneck, 64, 3)
+        self.layer2 = self._make_layer(Bottleneck, 128, 4, stride=2)
+        self.layer3 = self._make_layer(Bottleneck, 256, 6, stride=2)
+        self.layer4 = self._make_layer(Bottleneck, 512, 3, stride=2)
+
+        # Classifier
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_size * 2, hidden_size),
+            nn.Linear(512 * Bottleneck.expansion, 256),
             nn.ReLU(),
-            nn.Linear(hidden_size, num_chars * num_classes)
+            nn.Linear(256, num_chars * num_classes)
         )
+
         self.num_chars = num_chars
         self.num_classes = num_classes
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.max_pool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
         # Global Average Pooling (GAP) layer
         x = nn.functional.adaptive_avg_pool2d(x, (1, 1))
-
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
 
         x = x.view(-1, self.num_chars, self.num_classes)
         return x
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        layers = []
+
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels * block.expansion)
+            )
+
+        layers.append(block(self.in_channels, out_channels, i_downsample=downsample, stride=stride))
+        self.in_channels = out_channels * block.expansion
+
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))  # noqa: PERF401
+
+        return nn.Sequential(*layers)
