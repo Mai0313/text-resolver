9a37c439f3335b19d8ca2ffbc149c6eb7be6b85a
mai0313 <mai@mai0313.com>
2023-08-31 13:39:46 +0800

    feat: remove unused package and change train images

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install dvc dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX timm==0.4.12 rootutils wget
-pip install autoroot autorootcwd ipykernel lightning matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118;
+pip install dvc dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX rootutils wget;
+pip install autoroot autorootcwd ipykernel lightning==2.0.8 matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
 pause
