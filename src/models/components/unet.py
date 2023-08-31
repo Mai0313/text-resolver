52de90d7fbde0729ed33af117c12de2168c29b64
mai0313 <mai@mai0313.com>
2023-08-31 14:02:25 +0800

    pre-commit

--- src/models/components/unet.py
+++ src/models/components/unet.py
@@ -1,76 +1,59 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-
-class UNetBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.block(x)
 
 class CaptchaUNet(nn.Module):
     def __init__(self,
                  in_channels: int = 1,
-                 kernel_size: int = 2,
+                 kernel_size: int = 3,
                  stride: int = 1,
-                 num_classes: int=36,
-                 num_chars: int=5
+                 padding: int = 1,
+                 num_classes: int = 36,
+                 num_chars: int = 5,
+                 hidden_size: int = 256
                  ):
         super().__init__()
 
-        self.kernel_size = kernel_size
-        self.stride = stride
+        # Encoder (Downsampling path)
+        self.enc1 = self.conv_block(in_channels, hidden_size // 8, kernel_size, stride, padding)
+        self.enc2 = self.conv_block(hidden_size // 8, hidden_size // 4, kernel_size, stride, padding)
+
+        # Decoder (Upsampling path)
+        self.dec1 = self.conv_block(hidden_size // 4, hidden_size // 8, kernel_size, stride, padding)
+
+        # Classifier (The input dimension is set later dynamically)
+        self.classifier = nn.Sequential(
+            nn.Linear(0, hidden_size),  # Input dimension will be set dynamically
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_chars * num_classes)
+        )
+
         self.num_chars = num_chars
         self.num_classes = num_classes
 
-        self.enc1 = UNetBlock(in_channels, 64)
-        self.enc2 = UNetBlock(64, 128)
-        self.enc3 = UNetBlock(128, 256)
-        self.enc4 = UNetBlock(256, 512)
-
-        self.pool = nn.MaxPool2d(self.stride)
-
-        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=self.kernel_size, stride=self.stride)
-        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=self.kernel_size, stride=self.stride)
-        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=self.kernel_size, stride=self.stride)
-
-        self.dec3 = UNetBlock(512, 256)
-        self.dec2 = UNetBlock(256, 128)
-        self.dec1 = UNetBlock(128, 64)
-
-        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.final_conv = nn.Conv2d(64, num_classes * num_chars, kernel_size=1)
+    def conv_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
 
     def forward(self, x):
-        enc1 = self.enc1(x)
-        enc2 = self.enc2(self.pool(enc1))
-        enc3 = self.enc3(self.pool(enc2))
-        enc4 = self.enc4(self.pool(enc3))
+        # Encoder
+        x1 = self.enc1(x)
+        x2 = self.enc2(x1)
 
-        upconv3_out = self.upconv3(self.pool(enc4))
-        pooled_enc3 = self.pool(enc3)
-        if upconv3_out.shape[2:] != pooled_enc3.shape[2:]:
-            diffY = pooled_enc3.size()[2] - upconv3_out.size()[2]
-            diffX = pooled_enc3.size()[3] - upconv3_out.size()[3]
-            upconv3_out = F.pad(upconv3_out, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        # Decoder
+        x = self.dec1(x2)
 
-        dec3 = self.dec3(torch.cat([self.upconv3(self.pool(enc4)), self.pool(enc3)], 1))
-        dec2 = self.dec2(torch.cat([self.upconv2(self.pool(dec3)), self.pool(enc2)], 1))
-        dec1 = self.dec1(torch.cat([self.upconv1(self.pool(dec2)), self.pool(enc1)], 1))
+        # Dynamically calculate flat_size based on the output from the decoder
+        self.flat_size = x.shape[1] * x.shape[2] * x.shape[3]
 
-        avg_pooled = self.global_avg_pool(dec1)
-        out = self.final_conv(avg_pooled)
-        out = out.view(-1, self.num_chars, self.num_classes)
-        return out
+        # Update the input dimension for the first Linear layer in classifier
+        self.classifier[0] = nn.Linear(self.flat_size, self.classifier[0].out_features).to(x.device)
 
+        # Flatten and Classifier
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
 
-if __name__ == "__main__":
-    _ = CaptchaUNet()
+        x = x.view(-1, self.num_chars, self.num_classes)
+        return x
