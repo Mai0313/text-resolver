d382d9df05e5dd1e50322bb22207c1994fe69b1b
do <mai@mai0313.com>
2023-12-11 05:58:33 +0800

    Update model architecture in md4.yaml and transformer.py

--- src/models/components/transformer.py
+++ src/models/components/transformer.py
@@ -1,102 +1,75 @@
-import torch.nn as nn
-from lightning import LightningModule
+import math
+
+import torch
+from einops.layers.torch import Rearrange
+from pytorch_lightning import LightningModule
+from torch import nn
+from torchvision.transforms import Compose, Resize, ToTensor
 
 
-class TransformerBlock(LightningModule):
-    def __init__(self, d_model, nhead, dim_feedforward):
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels=1, patch_size=16, emb_size=768, img_size=96):
         super().__init__()
-        self.attention = nn.MultiheadAttention(d_model, nhead)
-        self.feed_forward = nn.Sequential(
-            nn.Linear(d_model, dim_feedforward), nn.ReLU(), nn.Linear(dim_feedforward, d_model)
+        self.projection = nn.Sequential(
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange("b e h w -> b (h w) e"),
         )
-        self.layer_norm1 = nn.LayerNorm(d_model)
-        self.layer_norm2 = nn.LayerNorm(d_model)
 
     def forward(self, x):
-        attn_output, _ = self.attention(x, x, x)
-        x = x + attn_output
-        x = self.layer_norm1(x)
-        ff_output = self.feed_forward(x)
-        x = x + ff_output
-        x = self.layer_norm2(x)
+        x = self.projection(x)
         return x
 
 
-class CaptchaNet(nn.Module):
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x + self.pe[: x.size(0)]
+        return self.dropout(x)
+
+
+class TransformerCaptchaSolver(LightningModule):
     def __init__(
         self,
-        in_channels: int = 1,
-        kernel_size: int = 3,
-        stride: int = 1,
-        padding: int = 1,
-        num_classes: int = 36,
-        num_chars: int = 5,
-        hidden_size: int = 256,
+        in_channels=1,
+        patch_size=16,
+        emb_size=768,
+        img_size=96,
+        num_classes=36,
+        num_chars=5,
+        num_layers=6,
+        num_heads=12,
+        hidden_size=2048,
     ):
         super().__init__()
 
-        self.feature_extractor = nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                hidden_size // 8,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(
-                hidden_size // 8,
-                hidden_size // 4,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(
-                hidden_size // 4,
-                hidden_size // 2,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(
-                hidden_size // 2,
-                hidden_size,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
+        self.patch_embedding = PatchEmbedding(in_channels, patch_size, emb_size, img_size)
+        self.pos_encoder = PositionalEncoding(emb_size)
+
+        encoder_layers = nn.TransformerEncoderLayer(
+            emb_size, num_heads, hidden_size, batch_first=True
         )
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
 
-        d_model = hidden_size * 3 * 1  # 根據你的特徵提取器來調整
-        nhead = 4
-        dim_feedforward = 512
-
-        self.transformer_block = TransformerBlock(d_model, nhead, dim_feedforward)
-
-        self.classifier = nn.Sequential(
-            nn.Linear(d_model, hidden_size * 2),
-            nn.ReLU(),
-            nn.Linear(hidden_size * 2, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, num_chars * num_classes),
-        )
-
+        self.fc1 = nn.Linear(emb_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, num_classes * num_chars)
         self.num_chars = num_chars
         self.num_classes = num_classes
 
     def forward(self, x):
-        x = self.feature_extractor(x)
-        x = x.view(x.size(0), -1)
-        x = x.unsqueeze(1)  # 添加序列維度
-        x = self.transformer_block(x)
-        x = x.squeeze(1)  # 去除序列維度
-        x = self.classifier(x)
-        x = x.view(-1, self.num_chars, self.num_classes)
-        return x
+        x = self.patch_embedding(x)
+        x = self.pos_encoder(x)
+        x = self.transformer_encoder(x)
+        x = self.fc1(x[:, -1, :])
+        x = torch.relu(x)
+        x = self.fc2(x)
+        return x.view(-1, self.num_chars, self.num_classes)
