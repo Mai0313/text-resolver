4132b12dcf44b343346cea880b807d5af0af44e9
wei <weichenglee0313@gmail.com>
2024-09-20 21:54:37 +0800

    chore: update all cicd and using rye to control the dependicies

--- setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118;
-pip install dvc dvc-gdrive hydra-colorlog hydra-core hydra-optuna-sweeper tensorboard tensorboardX rootutils wget;
-pip install autoroot autorootcwd ipykernel lightning==2.0.8 matplotlib opencv-python opencv-python-headless Pillow==9.5.0 soundfile pyrootutils
-pause
--- start.bat
+++
@@ -1,1 +0,0 @@
-python src/train.py experiment=md2; python src/train.py experiment=md3; python src/train.py experiment=md4; python src/train.py experiment=md5; python src/train.py experiment=md6
